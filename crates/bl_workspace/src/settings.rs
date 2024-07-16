e3e7fb050520170669c4fa49ff2eb217767a0aae
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-15 20:58:20 -0500

    workspace: add builtin exclusion/inclusion patterns for file resolver settings

--- crates/bl_workspace/src/settings.rs
+++ crates/bl_workspace/src/settings.rs
@@ -31,10 +31,39 @@ impl FilePattern {
         Ok(())
     }
 }
+
 pub(crate) static EXCLUDE: &[FilePattern] = &[
+    FilePattern::Builtin(".bzr"),
+    FilePattern::Builtin(".direnv"),
+    FilePattern::Builtin(".eggs"),
+    FilePattern::Builtin(".git"),
+    FilePattern::Builtin(".git-rewrite"),
+    FilePattern::Builtin(".hg"),
+    FilePattern::Builtin(".ipynb_checkpoints"),
+    FilePattern::Builtin(".mypy_cache"),
+    FilePattern::Builtin(".nox"),
+    FilePattern::Builtin(".pants.d"),
+    FilePattern::Builtin(".pyenv"),
+    FilePattern::Builtin(".pytest_cache"),
+    FilePattern::Builtin(".pytype"),
+    FilePattern::Builtin(".ruff_cache"),
+    FilePattern::Builtin(".svn"),
+    FilePattern::Builtin(".tox"),
+    FilePattern::Builtin(".venv"),
+    FilePattern::Builtin(".vscode"),
+    FilePattern::Builtin("__pypackages__"),
+    FilePattern::Builtin("_build"),
+    FilePattern::Builtin("buck-out"),
+    FilePattern::Builtin("dist"),
+    FilePattern::Builtin("node_modules"),
+    FilePattern::Builtin("site-packages"),
+    FilePattern::Builtin("venv"),
 ];
 
 pub(crate) static INCLUDE: &[FilePattern] = &[
+    FilePattern::Builtin("*.html"),
+    FilePattern::Builtin("*.jinja"),
+    FilePattern::Builtin("*.twig"),
 ];
 
 #[derive(Debug, Clone, Default)]
