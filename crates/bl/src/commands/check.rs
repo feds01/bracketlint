cb53187823b2f6db68b9277e6a40c0f212de0922
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-12-16 20:11:11 +0000

    bl: integrate the parser with `check` command

--- crates/bl/src/commands/check.rs
+++ crates/bl/src/commands/check.rs
@@ -1,11 +1,14 @@
 //! Implementation of the `check` command.
 
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 
 use anyhow::Result;
 use bl_diagnostics::Diagnostics;
-use bl_utils::timed;
-use bl_workspace::{resolver::find_files_in_paths, settings::Settings};
+use bl_parse::{parse_source, ParseQuery, ParseResult};
+use bl_reporting::{default_renderer, ReportWriter};
+use bl_utils::{stream::CompilerOutputStream, timed};
+use bl_workspace::{resolver::find_files_in_paths, settings::Settings, WorkspaceBuilder};
+use log::info;
 
 pub fn check(files: &[PathBuf], settings: Settings) -> Result<Diagnostics> {
     // Firstly, we need to discover all of the files in the provided paths.
@@ -20,12 +23,52 @@ pub fn check(files: &[PathBuf], settings: Settings) -> Result<Diagnostics> {
         return Ok(Diagnostics::default());
     }
 
+    info!("found {} files", files.len());
+
+    // @@Todo: move this out to above as this isn't the right place to
+    // build the workspace.
+    let builder = WorkspaceBuilder::new()
+        .with_settings(settings)
+        .with_stdout(CompilerOutputStream::stdout())
+        .with_stderr(CompilerOutputStream::stderr());
+    let mut workspace = builder.build();
+    let renderer = default_renderer();
+
     // @@Todo: integrate a cache system here, we should be able to avoid re-linting
     // already existent files and just skip them.
+    // Now iterate the files, parse them and lint them.
+    //
+    // @@Todo: we could fairly easily parallelize this operation, but we need to
+    // first add `salsa` to the project. For now, we'll just lint them sequentially
+    // and then parallelize it later.
+    for file in files {
+        match file {
+            Ok(file) => {
+                // We first need to convert this to a workspace member.
+                let contents = fs::read_to_string(file.path())?;
+                let id = workspace.members.reserve_member(file.into_path(), contents);
+                let member = workspace.members.member(id);
 
-    // Now iterate the files in parallel, parse them and lint them.
+                let ParseResult { node, diagnostics } =
+                    parse_source(ParseQuery::new(id, &member.contents));
 
-    println!("found {} files", files.len());
+                // Print any shown diagnostics
+                if !diagnostics.is_empty() {
+                    for diagnostic in diagnostics.iter() {
+                        let reporter = ReportWriter::new(&renderer, diagnostic);
+                        println!("{}", reporter);
+                    }
+                }
+
+                let member = workspace.members.member_mut(id);
+                member.document = node;
+            }
+            Err(err) => {
+                // @@todo: Create a diagnostic for the error, and print it!
+                println!("error: {}", err);
+            }
+        }
+    }
 
     Ok(Diagnostics::default())
 }
